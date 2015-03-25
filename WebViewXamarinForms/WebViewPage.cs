df437ee2856cb42f1f9eaaabbf88e6ef753d1b4f
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 15:31:14 -0400

    How to implement a webview in Xamarin Forms

---
+++ WebViewXamarinForms/WebViewPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace WebViewXamarinForms
+{
+	public class WebViewPage : ContentPage
+	{
+		public WebViewPage ()
+		{
+			Label lbl_header = new Label {
+				Text = "WebView",
+				HorizontalOptions = LayoutOptions.Center
+			};
+			WebView webview = new WebView {
+				Source = "http://google.com",
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+			this.Content = new StackLayout {
+				Children = {
+					 webview
+				}
+			};
+		
+		}
+	}
+}
+
+
